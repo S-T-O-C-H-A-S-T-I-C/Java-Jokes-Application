e6ae888aa1ea2d5007fd1ddc19df0b9e7327f81f
Somanshu Singla <ssomanshus@gmail.com>
2019-01-31 17:23:19 +0530

    Initial Commit

---
+++ src/main/java/com/example/JokesApp/JokesApplication.java
@@ -0,0 +1,14 @@
+package com.example.JokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/JokesApp/JokesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.JokesApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JokesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
